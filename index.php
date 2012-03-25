74d683f8d942cd9ebaa7a05fd9216de34b59487c
Ben Bowler <ben@benbowler.com>
2012-03-25 10:47:34 +0100

    Added homepage

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+	<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+		<title>Crack the Code - Super spy game</title>
+	</head>
+	<body>
+		<div>
+			<!-- AddThis Button BEGIN -->
+			<div class="addthis_toolbox addthis_default_style ">
+				<a class="addthis_button_facebook_like" fb:like:layout="button_count"></a>
+				<a class="addthis_button_tweet"></a>
+				<a class="addthis_button_google_plusone" g:plusone:size="medium"></a>
+				<a class="addthis_counter addthis_pill_style"></a>
+			</div>
+			<script type="text/javascript" src="http://s7.addthis.com/js/250/addthis_widget.js#pubid=ra-4f196e396be69250"></script>
+			<!-- AddThis Button END -->
+		</div>
+		<object type="application/x-shockwave-flash" data="Crack the Code.swf" width="100%" height="100%">
+			<param name="movie" value="Crack the Code.swf" />
+		</object>
+	</body>
+</html>
